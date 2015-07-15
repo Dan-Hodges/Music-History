b5197327f44905f685a5d2c937fb8ab36eae1757
Dan-Hodges <dhdgs690@gmail.com>
2015-07-15 09:06:08 -0500

    added push and unshift tracks to array

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+songs.unshift("On the Beach > by Neil Young on the album On the Beach");
+songs.push("Little Wing > by Jimi Hendrix on the album Axis: Bold as Love")
 
 
 songHolder = document.getElementById("song1");
