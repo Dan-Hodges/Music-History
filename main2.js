00f8dbb697b5553fbcef53e6a9504cb0c3ed005c
Dan-Hodges <dhdgs690@gmail.com>
2015-07-29 13:48:14 -0500

    added delete button

--- main2.js
+++ main2.js
@@ -1,11 +1,11 @@
-var songData = "";
 $(document).ready(function() {
+var songData = "";
   $.ajax({
     url: "ajaxtext.json"
   }).done(function(data) {
       console.log(data);
       for (var i = 0; i < data.songs.length; i++) {
-        songData += "<div class='songs'>" + "<h2 id='songTitle'>" + data.songs[i].title + "</h2>";
+        songData += "<div class='songs' id='songs'>" + "<h2 id='songTitle'>" + data.songs[i].title +"</h2>" + "<button id='delete' type='button'>" + "Delete" + "</button>";
         songData += "<ul id='songlist'>";
         songData += "<li id='artist'>" + data.songs[i].artist + "</li>";
         songData += "<li id='album'>" + data.songs[i].album + "</li>";
@@ -14,6 +14,27 @@ $(document).ready(function() {
       }
       $('.songinfo').html(songData);
       console.log(songData);
-  })
+  });
+$('button').click(function() {
+      $.ajax({
+      url: "list2.json"
+    }).done(function(data) {
+        console.log(data);
+        for (var i = 0; i < data.songs2.length; i++) {
+          songData += "<div class='songs' id='songs'>" + "<h2 id='songTitle'>" + data.songs2[i].title +"</h2>" + "<button id='delete' type='button'>" + "Delete" + "</button>";
+          songData += "<ul id='songlist'>";
+          songData += "<ul id='songlist'>";
+          songData += "<li id='artist'>" + data.songs2[i].artist + "</li>";
+          songData += "<li id='album'>" + data.songs2[i].album + "</li>";
+          songData += "</ul>";
+          songData += "</div>";
+        }
+        $('.songinfo').html(songData);
+        console.log(songData);  
+  });
+  });
+$(document).on("click", "#delete", function() {
+    console.log("you click5ed the delete button");
+  });
 });
 
